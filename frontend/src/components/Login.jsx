6af102b4f69808fca8cd3662f6457d6c9d9272c4
Ajay Singh Adhikari <ajaysinghadhikari697@gmail.com>
2025-07-21 02:36:54 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       password: data.password,
     };
     await axios
-      .post("https://bookstore-backend-wdxz.onrender.com/login", userInfo)
+      .post("https://bookstore-backend-wdxz.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
