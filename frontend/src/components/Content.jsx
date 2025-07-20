648efde59008e87f4654bc08c2ba41f3ce2039f8
Ajay Singh Adhikari <ajaysinghadhikari697@gmail.com>
2025-07-21 02:16:58 +0530

    Update Content.jsx

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -9,7 +9,7 @@ const Content = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstore-backend-wdxz.onrender.com");
         setBook(res.data);
       } catch (error) {
         console.log("Error: ", error);
