eb27ff5a39b4776ed5ec58129e5649eccf8d722b
Ajay Singh Adhikari <ajaysinghadhikari697@gmail.com>
2025-07-21 02:18:32 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -18,7 +18,7 @@ const Signup = () => {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://bookstore-backend-wdxz.onrender.com", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
